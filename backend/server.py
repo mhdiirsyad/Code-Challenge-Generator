d1a8853baa63bc543a11efc9797a011ce8530279
mhdiirsyad <irsyadcahkalipucang@gmail.com>
2025-06-15 12:15:20 +0700

    fix database

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,6 @@
-from src.app import app
+# from src.app import app
 
-if __name__ == "__main__":
-    import uvicorn
+# if __name__ == "__main__":
+#     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
